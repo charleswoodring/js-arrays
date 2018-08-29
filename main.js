dab28a3daaebb6010b4f36ff141e6bd206ca686b
Charles Woodring <charles.s.woodring@gmail.com>
2018-08-28 19:09:10 -0500

    arrays

---
+++ main.js
@@ -0,0 +1,27 @@
+const cats = ['fluffy', 'spike', 'killer'];
+console.log('1st cat', cats[0]);
+
+const favoriteAnimals = 'cow,cat,bear,dog';
+const favoriteAnimalArray = favoriteAnimals.split() // ['c','o','w']
+console.log('favoriteAnimalArray', favoriteAnimalArray);
+
+const numArray = [1, 2, 3, 4, 5];
+const newNum = numArray.join('cow');
+console.log('newNum', newNum);
+
+// mini challenge
+const palChecker = (word) => {
+    const wordArray = word.split('').join('');
+    const reverseArray = word.split('').reverse().join('');
+ //   if(wordArray === reverseArray) {
+     if(word === reverseArray) {
+        console.log(`${word} IS a palindrome`);
+    } else {
+        console.log(`${word} NOT a palindrome`);
+    }
+// yes if its a palindrome
+// no if its nota palindrome
+};
+palChecker('mom'); //yes
+palChecker('monkeybutt') // no
+palChecker('racecar') // yes
